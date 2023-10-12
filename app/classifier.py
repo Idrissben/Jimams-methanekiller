b6ea476a7eb7f61b1f12b3aeb70776c1706c6f17
Ajouad <96442128+Ajouad@users.noreply.github.com>
2023-10-12 18:17:54 +0200

    Docstring classifier maj

--- app/classifier.py
+++ app/classifier.py
@@ -9,13 +9,14 @@ def resnet18(freeze_parameters: bool = True) -> nn.Module:
     Load a pre-trained ResNet-18 model with the option to freeze its parameters and modify the output layer.
 
     Args:
-        freeze_parameters (bool, optional): If True, freeze the parameters of the loaded ResNet-18 model.
+        freeze_parameters (bool, optional): If True, freeze the parameters of the loaded ResNet-18 model. 
             Defaults to True.
 
     Returns:
         model (nn.Module): The ResNet-18 model with modified output layer.
     """
-    model = torch.hub.load("pytorch/vision:v0.10.0", "resnet18", pretrained=True)
+    model = torch.hub.load('pytorch/vision:v0.10.0',
+                           'resnet18', pretrained=True)
 
     if freeze_parameters:
         for param in model.parameters():
@@ -27,6 +28,13 @@ def resnet18(freeze_parameters: bool = True) -> nn.Module:
 
 class Classifier:
     def __init__(self, model_path):
+        """
+        Initializes the Classifier with a specified model path.
+
+        Parameters:
+            model_path (str): Path to the pretrained model weights. The provided path should 
+                              point to the ResNet-18 state dict compatible weights.
+        """
         # Create a new ResNet-50 model with random weights
         self.model = resnet18()
 
@@ -34,18 +42,30 @@ class Classifier:
         # Set the model to evaluation mode
         self.model.eval()
         # Define the image transformations to be applied
-        self.transform = transforms.Compose(
-            [
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                transforms.Normalize(
-                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-                ),
-            ]
-        )
+        self.transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                                 0.229, 0.224, 0.225])
+        ])
 
     def classify_image(self, image_path):
+        """
+        Classifies an input image and returns the predicted class ID and its probability.
+
+        Args:
+            image_path (str): Path to the input image. The image should be in a format 
+                              compatible with PIL (e.g., JPG, PNG).
+
+        Returns:
+            int: The predicted class ID. This ID is based on the order of classes used during model training.
+            float: The probability associated with the predicted class.
+
+        Note:
+            - The input image should be RGB.
+            - It's recommended that the input image is of high quality for more accurate classification.
+        """
         # Load the image from the specified file
         image = Image.open(image_path)
         # Apply the specified transformations to the image
@@ -57,7 +77,4 @@ class Classifier:
             output = self.model(image)
             probabilities = torch.nn.functional.softmax(output[0], dim=0)
         # Return the predicted class and its probability
-        return (
-            torch.argmax(probabilities).item(),
-            probabilities[torch.argmax(probabilities)].item(),
-        )
+        return torch.argmax(probabilities).item(), probabilities[torch.argmax(probabilities)].item()
